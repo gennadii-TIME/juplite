b6fa0c196c14dc518b7638162a97d3472915979c
gennadii-TIME <gennadiistepanov@gmail.com>
2025-11-19 19:57:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Juplite common JS
+// Right now we only set the current year in the footer on pages that include #year.
+
+document.addEventListener("DOMContentLoaded", function () {
+  var yearEls = document.querySelectorAll('#year');
+  var year = new Date().getFullYear();
+  yearEls.forEach(function (el) {
+    el.textContent = year;
+  });
+});
